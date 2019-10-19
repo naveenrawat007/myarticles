d915c8ca82c14d8c57436a7e207834e80d581a79
naveen rawat <naveenrawat808@gmail.com>
2019-10-19 16:55:54 +0530

    add gem and user controllers for registraion

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace "v1" do
-      devise_for :users
       resources :articles
       resources :people
+      devise_for :users, controllers: { registrations: "api/v1/user/registrations" }
     end
   end
 end
