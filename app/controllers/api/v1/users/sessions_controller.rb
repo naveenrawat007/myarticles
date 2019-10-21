fc151d29ec021ffab27b304d6c4d66fa89c5e74d
naveen rawat <naveenrawat808@gmail.com>
2019-10-21 18:45:33 +0530

    cors issue fixed

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -4,13 +4,12 @@ module Api
       def create
         @user = User.find_by(email:params[:email])
         if @user&.valid_password?(params[:password])
-          token = JWT.encode({user_id: @user.id}, 'HS256')
+          token = JWT.encode({user_id: @user.id}, 'HS512')
           render json: {token: token, user_id: @user.id}
         else
           render json: {error:"invalid username/password"},status: :unauthorized
         end
       end
-
     end
   end
 end
