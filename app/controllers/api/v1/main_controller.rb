0de7bd85236097e7cc96d5dbc411548dc140324d
vinaymehta <vinay.ymca@gmail.com>
2019-10-21 17:12:04 +0530

    fixed jwt decode error

--- app/controllers/api/v1/main_controller.rb
+++ app/controllers/api/v1/main_controller.rb
@@ -1,18 +1,18 @@
 class MainController < ActionController::API
   def authorize_request
     header = request.headers['Authorization']
-    user = User.find(header)
-    if user.present?
-      render json: { current_user_id: user.id } 
-    end
-    # header = header.split(' ').last if header
-    # begin
-    #   @decoded = JWT.decode(header)
-    #   @current_user = User.find(@decoded[:user_id])
-    # rescue ActiveRecord::RecordNotFound => e
-    #   render json: { errors: e.message }, status: :unauthorized
-    # rescue JWT::DecodeError => e
-    #   render json: { errors: e.message }, status: :unauthorized
+    # user = User.find(header)
+    # if user.present?
+    #   render json: { current_user_id: user.id }
     # end
+    header = header.split(' ').last if header
+    begin
+      @decoded = JWT.decode(header,nil,false)
+      @current_user = User.find(@decoded.first['user_id'])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
   end
 end
