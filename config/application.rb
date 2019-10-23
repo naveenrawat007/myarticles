fb434304e6d1b3f2fc70635c25e25ed753d58bc8
naveen rawat <naveenrawat808@gmail.com>
2019-10-23 11:32:28 +0530

    files added

--- config/application.rb
+++ config/application.rb
@@ -24,10 +24,13 @@ module Myarticles
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: :any
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
